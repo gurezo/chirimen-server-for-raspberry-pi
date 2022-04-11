4df9f41ddd25869bb5dd9a567d9568e115657f32
akihiko.kigure <akihiko.kigure@gmail.com>
2022-04-11 22:11:51 +0900

    api 仮実装

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,21 +1,92 @@
 import { Request, Response } from "express";
+import { GPIOAccess } from "node-web-gpio";
+const { requestGPIOAccess } = require("node-web-gpio");
 
-interface HelloResponse {
-  hello: string;
-}
+// interface HelloResponse {
+//   hello: string;
+// }
+// type HelloBuilder = (name: string) => HelloResponse;
+// const helloBuilder: HelloBuilder = (name) => ({ hello: name });
 
-type HelloBuilder = (name: string) => HelloResponse;
+// type GPIOAccessBuilder = () => void;
+// const gpioAccessBuilder: GPIOAccessBuilder = () => {};
 
-const helloBuilder: HelloBuilder = (name) => ({ hello: name });
+let gpioAccess: GPIOAccess;
+let ports: any;
 
 export const rootHandler = (_req: Request, res: Response) => {
   return res.send("Hello, Your API is working!!");
 };
 
-export const helloHandler = (req: Request, res: Response) => {
-  const { params } = req;
-  const { name = "World" } = params;
-  const response = helloBuilder(name);
+// export const helloHandler = (req: Request, res: Response) => {
+//   const { params } = req;
+//   const { name = "World" } = params;
+//   const response = helloBuilder(name);
 
-  return res.json(response);
+//   return res.json(response);
+// };
+
+const okRsponse = { status: "ok" };
+const errorRsponse = (error: any) => ({
+  status: "error",
+  error,
+});
+
+const customThrower = (message: string) => {
+  throw Error(message);
 };
+
+// const isPortsUndefined = () => ({ throw new Error("posrt is undefined"); });
+const commonHandler = (func: Function): { status: string; error?: any } => {
+  try {
+    func();
+    return okRsponse;
+  } catch (error) {
+    return errorRsponse(error);
+  }
+};
+
+export const gpioAccessHandler = async (req: Request, res: Response) => {
+  let response = {};
+  commonHandler(await requestGPIOAccess());
+
+  // try {
+  //   gpioAccess = await requestGPIOAccess();
+  //   response = okRsponse;
+  // } catch (error) {
+  //   response = errorRsponse(error);
+  // } finally {
+  //   return res.json(response);
+  // }
+};
+
+export const gpioPortsGetHandler = async (req: Request, res: Response) => {
+  const { params } = req;
+  let response = {};
+  // if (!ports) {
+  //   throw new Error("posrt is undefined");
+  // }
+  try {
+    ports = !ports
+      ? customThrower("posrt is undefined")
+      : gpioAccess.ports.get(parseInt("26", 10));
+    response = okRsponse;
+  } catch (error) {
+    response = errorRsponse(error);
+  } finally {
+    return res.json(response);
+  }
+};
+
+// export const gpioPortWriteHandler = async (req: Request, res: Response) => {
+//   const { params } = req;
+//   let response = {};
+//   try {
+//     port.write(1);
+//     response = okRsponse;
+//   } catch (error) {
+//     response = errorRsponse(error);
+//   } finally {
+//     return res.json(response);
+//   }
+// };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
-import { helloHandler, rootHandler } from "./handlers";
+import {
+  /** helloHandler, */ gpioAccessHandler,
+  rootHandler,
+} from "./handlers";
 
 const app = express();
 const port = process.env.PORT || "8000";
 
 app.get("/", rootHandler);
-app.get("/hello/:name", helloHandler);
+// app.get("/hello/:name", helloHandler);
+
+app.get("/gpio/access", gpioAccessHandler);
 
 app.listen(port, () => {
   return console.log(`Server is listening on ${port}`);
